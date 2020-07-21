b69b502134f463fee56437a90de045f72c95413e
azkusi <46655121+azkusi@users.noreply.github.com>
2020-07-21 12:24:28 +0100

    Add files via upload

---
+++ sendDB.php
@@ -0,0 +1,241 @@
+<?php
+
+//=================================================================================
+//                          CONNECTION TO DATABASE
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+// $conn = mysqli_connect("localhost", "coverage", "antenna", "coverage");
+
+
+$servername = "localhost";
+$username = "coverage";
+$password = "antenna";
+$dbname = "coverage";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if(!$conn){
+
+    echo 'Connection error ' . mysqli_connect_error();
+}
+else{
+echo "connection made \n";
+}
+
+//=================================================================================
+//PLACING STRING OF DRAWN COORDINATES INTO DATABASE AND CONVERTING TO ARRAY OF INTEGERS
+
+$coordinates = (isset($_POST['coordinates'])) ? $_POST['coordinates'] : 'no name';
+
+
+
+$name = 'shapes';
+
+$sql = "INSERT INTO drawings (name, coords) VALUES ('$name', '$coordinates')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record into drawings created successfully \n";
+}
+
+else {
+    header("together.html?=notsuccessful");
+//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+$sql1 = "SELECT * FROM drawings WHERE coords='$coordinates'";
+
+$result1 = $conn->query($sql1);
+
+//find array just added by using 'select ID where coords == $coordinates' place in variable
+if ($result1 = $conn->query($sql1)) {
+    while($row = $result1->fetch_assoc()) {
+        echo "ID is: " . $row["ID"] . "\n";
+        $coordID = $row["ID"];
+    }
+}
+else{
+    echo "nahh didn't work \n";
+}
+
+//convert coordinates array from string of strings to array of integers
+$coordArray = array_map('intval', explode(',', $coordinates));
+
+echo $coordArray[0] . "\n";
+
+
+//=================================================================================
+//   FINDING COORDINATES THAT WILL DRAW LARGEST RECTANGLE FROM COORDINATE ARRAY
+
+//find the lowest x value and corresponding lowest y coordinate (bottom left corner)
+//find the largest x value and corresponding largest y coordinate (top right corner)
+//find the lowest x value and corresponding largest y coordinate (top left corner)
+//find the largest x value and corresponding lowest y coordinate (bottom right corner)
+
+
+
+//loop through the array just added, using 'select coords where ID == above variable'
+$smallestX = [1000000000];
+$smallestY = [0];
+
+$largestX = [0];
+$largestY = [1000000000];
+
+$smallestXpair = [0];
+$smallestYpair = [0];
+$largestXpair = [0];
+$largestYpair = [0];
+
+for ($x = 0; $x < (count($coordArray) - 2); $x+=2) {
+   //find smallest X
+    if ($coordArray[$x] < $smallestX[0]){
+        //replace smallestX array element with new value
+        $smallestX[0] = $coordArray[$x];
+    }
+    //find largest X
+    else if ($coordArray[$x] > $largestX[0]){
+        //replace largestX array element with new value
+        $largestX[0] = $coordArray[$x];
+            }
+
+    else{
+        continue;
+    }
+
+    }
+
+
+//find smallestY and largestY
+for ($y = 1; $y < (count($coordArray) - 1); $y+=2) {
+   //find smallest Y
+    if ($coordArray[$y] > $smallestY[0]){
+        //replace smallestY array element with new value
+        $smallestY[0] = $coordArray[$y];
+    }
+    //find largest Y
+    else if ($coordArray[$y] < $largestY[0]){
+        //replace largestY array element with new value
+        $largestY[0] = $coordArray[$y];
+            }
+
+    else{
+        continue;
+    }
+
+    }
+
+
+
+
+for ($x = 0; $x < (count($coordArray) - 2); $x+=2) {
+   //find smallestXpair
+    if ($coordArray[$x] == $smallestX[0]){
+        //set smallestXpair
+        $smallestXpair[0] = $coordArray[$x + 1];
+    }
+    //find largestXpair
+    else if ($coordArray[$x] == $largestX[0]){
+        //set largestXpair
+        $largestXpair[0] = $coordArray[$x + 1];
+            }
+
+    else{
+        continue;
+    }
+
+    }
+
+
+
+//find coordinate pairs for limiting x/y values
+for ($y = 1; $y < (count($coordArray) - 1); $y+=2) {
+   //find smallestYpair
+    if ($coordArray[$y] == $smallestY[0]){
+        //set smallestYpair
+        $smallestYpair[0] = $coordArray[$y - 1];
+    }
+    //find largest Y
+    else if ($coordArray[$y] == $largestY[0]){
+        //set smallestYpair
+        $largestYpair[0] = $coordArray[$y - 1];
+            }
+
+    else{
+        continue;
+    }
+
+    }
+
+
+//echo "The smallest X is: " . $smallestX[0] . "\n";
+
+
+//==================================================================================
+//         PLACING RECTANGLE 4 COORDINATE CORNERS INTO DATABASE TABLES
+
+
+//put together coordinates of corners
+
+$LeftCoord = [$smallestX[0], $smallestXpair[0]];
+$TopCoord = [$largestYpair[0], $largestY[0]];
+$RightCoord = [$largestX[0], $largestXpair[0]];
+$BottomCoord = [$smallestYpair[0], $smallestY[0]];
+
+echo "left coordinate is: " . $LeftCoord[0] . "," . $LeftCoord[1] . "\n";
+echo "top coordinate is: " . $TopCoord[0] . "," . $TopCoord[1] . "\n";
+echo "right coordinate is: " . $RightCoord[0] . "," . $RightCoord[1] . "\n";
+echo "bottom coordinate is: " . $BottomCoord[0] . "," . $BottomCoord[1] . "\n";
+
+
+
+$LeftCoord = implode(",", $LeftCoord);
+$TopCoord = implode(",", $TopCoord);
+$RightCoord = implode(",", $RightCoord);
+$BottomCoord = implode(",", $BottomCoord);
+
+
+//calculate area and perimeter
+
+//$area = () * ()
+//$perimeter = () + ()
+
+//place coordinates together and place into...
+//dimensions table for dimensions of squares, and in perimarea table
+
+
+$sql3 = "INSERT INTO dimensions (shapeID, TopCoord, RightCoord, BottomCoord, LeftCoord)
+VALUES ($coordID, '$TopCoord', '$RightCoord', '$BottomCoord', '$LeftCoord')";
+
+//$sql4 = "INSERT INTO perimarea (perimeter, area) VALUES ('$perimeter', '$area')";
+
+if (mysqli_query($conn, $sql3)) {
+  echo "New record into dimensions created successfully \n";
+}
+else{
+echo("nope did not work matey \n");
+}
+
+
+//if (mysqli_query($conn, $sql)) {
+//  echo "New record into perimarea created successfully \n";
+//}
+
+
+//now draw on front end this square
+
+mysqli_close($conn);
+
+
+
+
+
+
+
+
+
+?>
---
+++ updatePropModel.php
@@ -0,0 +1,58 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+// $conn = mysqli_connect("localhost", "coverage", "antenna", "coverage");
+
+
+$servername = "localhost";
+$username = "coverage";
+$password = "antenna";
+$dbname = "coverage";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if(!$conn){
+
+    echo 'Connection error ' . mysqli_connect_error();
+}
+//else{
+//echo 'connection made';
+//}
+
+
+
+
+$result = mysqli_query($conn, "SELECT * FROM perimarea WHERE shapeID = (SELECT MAX(ID) FROM drawings WHERE name = 'shapes')");
+
+$data = array();
+while ($row = mysqli_fetch_object($result))
+{
+    //array_push($data, $row);
+    echo  json_encode($row);
+}
+
+//echo $data;
+//echo  json_encode($data);
+
+
+
+
+
+
+mysqli_close($conn);
+
+
+// $q = $_POST['str'];
+
+
+
+
+
+
+
+?>
