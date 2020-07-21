d2679837b9c23275f6f4edad0b6799e27ef57085
azkusi <46655121+azkusi@users.noreply.github.com>
2020-07-21 18:02:33 +0100

    Update updatePropModel.php

--- updatePropModel.php
+++ updatePropModel.php
@@ -4,26 +4,36 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-// $conn = mysqli_connect("localhost", "coverage", "antenna", "coverage");
+$dbname = 'vodamodel';
+$dbhost = '';
+$dbusername = '';
+$dbpassword = '';
 
-
-$servername = "localhost";
-$username = "coverage";
-$password = "antenna";
-$dbname = "coverage";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-
-if(!$conn){
-
-    echo 'Connection error ' . mysqli_connect_error();
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-//else{
-//echo 'connection made';
-//}
 
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+// This is for debugging only, to confirm whether the database connection is successfull.
+echo "<p>Success: A working connection to MySQL was made!</p>";
+echo "<p>The database is: $dbname</p>";
+echo "<p>The host is: = $dbhost</p>";
+echo "<p>Host information: " . mysqli_get_host_info($conn) . "</p>";
+echo "<p>The database username is: $dbusername</p>";
 
 
 
